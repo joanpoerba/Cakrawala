9399741e80b9a1144859b2e32eb062e2fd05661f
joanpurba <joanpurba4@gmail.com>
2023-12-19 22:34:00 +0700

    feat: fix the docusaurus config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ import { themes as prismThemes } from "prism-react-renderer";
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: "Hello",
+  title: "Ketjeh Nusantara",
   tagline: "Indonesia!",
   favicon: "img/icon.png",
 
