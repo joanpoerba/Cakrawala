6134e44087fb93542b9f0515fec4fb4032c735c1
joanpurba <joanpurba4@gmail.com>
2023-12-21 14:07:03 +0700

    feat: adding custom underline

--- src/components/HomepageFeatures/About.jsx
+++ src/components/HomepageFeatures/About.jsx
@@ -5,17 +5,40 @@ export default function Team() {
     <>
       <main className="w-full h-screen flex justify-center">
         <div className="lg:w-[70%] w-[90%] flex flex-col justify-center items-start">
-          <h1 className="customUnderline lg:text-4xl md:text-3xl text-2xl">
+          <h1
+            style={{
+              background:
+                "linear-gradient(180deg, rgba(255, 255, 255, 0) 50%, #916d4d 50%)",
+            }}
+            className="customUnderline lg:text-4xl md:text-3xl text-2xl"
+          >
             Hello Indonesia!
           </h1>
           <h2 className="lg:text-6xl md:text-5xl text-4xl md:my-10 my-5">
-            Ini Cakrawala! Cara baru untuk bereksplorasi setiap sudut Indonesia.
+            Ini{" "}
+            <span
+              style={{
+                background:
+                  "linear-gradient(180deg, rgba(255, 255, 255, 0) 50%, #916d4d 50%)",
+              }}
+            >
+              Cakrawala!
+            </span>{" "}
+            Cara baru untuk bereksplorasi setiap sudut Indonesia.
           </h2>
           <p className="md:text-xl text-base">
-            Selamat datang di situs web eksplorasi provinsi-provinsi Indonesia!
+            Selamat datang di{" "}
+            <span
+              style={{
+                background:
+                  "linear-gradient(180deg, rgba(255, 255, 255, 0) 50%, #916d4d 50%)",
+              }}
+            >
+              Cakrawala!
+            </span>{" "}
             Temukan keindahan alam, budaya, dan keunikan masing-masing daerah
-            dengan informasi lengkap dan gambar menarik. Mari jelajahi Indonesia
-            dari Sabang sampai Merauke, semua dalam genggamanmu.
+            dengan informasi lengkap dan gambar menarik. Mari eksplor setiap
+            sudut Indonesia.
           </p>
           <Link
             className="flex flex-row items-center gap-x-3 button button--secondary mt-10 button--sm shadow-2xl"
